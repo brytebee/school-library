63bdeb15755971917c53324ef9bb4672fc61aa23
brytebee <brytebee@gmail.com>
2022-04-04 18:04:02 +0100

    :white_check_mark: Add initialize
    Added the initialize method

--- src/person.rb
+++ src/person.rb
@@ -2,4 +2,10 @@ class Person
   attr_reader :id
   attr_writer :age
   attr_accessor :name
+
+  def initialize age, name = 'unknown', parent_permission = false
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
 end
