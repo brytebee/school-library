865db5ce2d47b05fdbf8ca8683c0d861957080fc
brytebee <brytebee@gmail.com>
2022-04-04 18:32:52 +0100

    :rotating_light: Fix linters
    Fixed linter errors

--- src/student.rb
+++ src/student.rb
@@ -1,8 +1,7 @@
 require_relative 'person'
 
 class Student < Person
-  
-  def initialize(age, name = 'unknown', parent_permission = false, classroom = false)
+  def initialize(age, name = 'unknown', parent_permission: false, classroom: false)
     super(age, name, parent_permission)
   end
 
