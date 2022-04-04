a4cefe9974acbf101a7b907a142b7327f50b300c
brytebee <brytebee@gmail.com>
2022-04-04 18:36:59 +0100

    :white_check_mark: Add can_use_service?
    Added can_use_service? method

--- src/teacher.rb
+++ src/teacher.rb
@@ -4,4 +4,8 @@ class Teacher < Person
   def initialize(age, name = 'unknown', parent_permission: false, specialization = 'Math')
     super(age, name, parent_permission)
   end
+
+  def can_use_services?
+    true
+  end
 end
