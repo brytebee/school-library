2d7e2def5cd0189a4b5667188c02afc047157f5b
brytebee <brytebee@gmail.com>
2022-04-04 18:36:11 +0100

    :white_check_mark: Extend initialize
    Extended to include specialization

--- src/teacher.rb
+++ src/teacher.rb
@@ -1,4 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
+  def initialize(age, name = 'unknown', parent_permission: false, specialization = 'Math')
+    super(age, name, parent_permission)
+  end
 end
