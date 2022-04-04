f69155167f6b5f025246f8e9fd383654e1ad8b23
brytebee <brytebee@gmail.com>
2022-04-04 18:16:17 +0100

    :white_check_mark: Add play_hooky
    Added play_hooky method

--- src/student.rb
+++ src/student.rb
@@ -5,4 +5,8 @@ class Student < Person
   def initialize(age, name = 'unknown', parent_permission = false, classroom = false)
     super(age, name, parent_permission)
   end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
