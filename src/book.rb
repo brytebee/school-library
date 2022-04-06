3ec25cbf6eafaa2e97df36ecc9fe68ae3d459756
brytebee <brytebee@gmail.com>
2022-04-06 02:05:31 +0100

    :rotating_light: Fixing stuff
    Fixed linters

--- src/book.rb
+++ src/book.rb
@@ -1,4 +1,4 @@
-class Book  
+class Book
   attr_accessor :title, :author
 
   def initialize(title, author)
--- src/classroom.rb
+++ src/classroom.rb
@@ -2,6 +2,6 @@ class Classroom
   attr_accessor :label
 
   def initialize(label)
-    @label = label    
+    @label = label
   end
 end
