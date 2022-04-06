0f03f6fbab1f03f009271524ad878f9987654961
brytebee <brytebee@gmail.com>
2022-04-06 14:52:50 +0100

    :white_check_mark: Add Book#add_rental as review specify
    Added a add_rental method to class Book

--- src/book.rb
+++ src/book.rb
@@ -1,3 +1,4 @@
+require_relative 'rental'
 class Book
   attr_accessor :title, :author
   attr_reader :rentals
@@ -6,5 +7,9 @@ class Book
     @title = title
     @author = author
     @rentals = []
+  end
+
+  def add_rental(person, date)
+    Rental.new(date, person,  self)
   end
 end
