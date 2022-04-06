392c303dc2f17a7b49e3be318fdd307a9fbd08bf
brytebee <brytebee@gmail.com>
2022-04-06 12:58:01 +0100

    :heavy_plus_sign: Add Student#classroom setter
    Added Student#classroom

--- src/student.rb
+++ src/student.rb
@@ -11,4 +11,9 @@ class Student < Person
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
+  end
 end
