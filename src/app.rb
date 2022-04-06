fd29d2cff22ecc2f53b262bd18bda94517076946
brytebee <brytebee@gmail.com>
2022-04-06 19:03:38 +0100

    :white_check_mark: Update app.rb
    Updated app.rb with user options

---
+++ src/app.rb
@@ -0,0 +1,9 @@
+const option = "
+1. List all books.
+2. List all people.
+3. Create a person (teacher or student, not a plain Person).
+4. Create a book.
+5. Create a rental.
+6. List all rentals for a given person id."
+
+puts option
