6310979ec9aa371bce9a9b67981b096ba4619f20
brytebee <brytebee@gmail.com>
2022-04-08 11:43:45 +0100

    :fire: Clean up
    Cleaned up

--- src/app.rb
+++
@@ -1,8 +0,0 @@
-welcome = 'Welcome to School Library App'
-option = "
-1. List all books.
-2. List all people.
-3. Create a person (teacher or student, not a plain Person).
-4. Create a book.
-5. Create a rental.
-6. List all rentals for a given person id."
