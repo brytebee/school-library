130025a3defef372e660dfd0ccdef7a1977f1411
brytebee <brytebee@gmail.com>
2022-04-08 11:01:04 +0100

    :white_check_mark: Add App#console_entry_point
    Added App#console_entry_point

--- src/classes/app.rb
+++ src/classes/app.rb
@@ -11,4 +11,17 @@ class App
     @people = []
     @rentals = []
   end
+
+  def console_entry_point
+    puts 'Welcome to my School Library!'
+    until list_of_options
+      input = gets.chomp
+      if input == '7'
+        puts 'Thank You for using my School Library!'
+        break
+      end
+
+      option input
+    end
+  end
 end
