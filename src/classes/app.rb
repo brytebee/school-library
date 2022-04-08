1d4ef7ba67245925505e371bd0870b22b4d75e3b
brytebee <brytebee@gmail.com>
2022-04-08 10:57:50 +0100

    :white_check_mark: Init app
    Added app init

--- src/classes/app.rb
+++ src/classes/app.rb
@@ -4,3 +4,11 @@ require_relative 'book'
 require_relative 'classroom'
 require_relative 'teacher'
 require_relative 'rental'
+
+class App
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+end
