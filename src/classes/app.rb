75da3de568e1bea563a47f743a860c3f8abddcf3
brytebee <brytebee@gmail.com>
2022-04-08 11:14:09 +0100

    :white_check_mark: Create rental
    Created rental

--- src/classes/app.rb
+++ src/classes/app.rb
@@ -94,4 +94,26 @@ class App
     puts 'Database is empty! Add a book.' if @books.empty?
     @books.each { |book| puts "[Book] Title: #{book.title}, Author: #{book.author}" }
   end
+
+  def create_rental
+    puts 'Select which book you want to rent by entering its number'
+    @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
+
+    book_id = gets.chomp.to_i
+
+    puts 'Select a person from the list by its number'
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+
+    person_id = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp.to_s
+
+    rental = Rental.new(date, @people[person_id], @books[book_id])
+    @rentals << rental
+
+    puts 'Rental created successfully'
+  end
 end
