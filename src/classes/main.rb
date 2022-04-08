818a4f4bcd5ac3d1ff9434803ea1d8de5b8e4453
brytebee <brytebee@gmail.com>
2022-04-08 11:19:53 +0100

    :white_check_mark: Add list all options
    Added options

--- src/classes/main.rb
+++ src/classes/main.rb
@@ -5,3 +5,15 @@ def main
   app = App.new
   app.console_entry_point
 end
+
+def list_of_options
+  puts
+  puts 'Choose an option by entering a number: '
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
