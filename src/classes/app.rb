5d83f861fbd4da805ad4ce5392b945442f56e2fa
banlonjones <banlonjones@go-groups.net>
2022-04-11 14:44:57 +0100

    list books, create_rentals and list_rentals refactored

--- src/classes/app.rb
+++ src/classes/app.rb
@@ -11,4 +11,46 @@ class App
     @people = []
     @rentals = []
   end
+
+  def list_all_books
+    puts
+    puts 'Database is empty! Add a book.' if @books.empty?
+    @books.each { |book| puts "[Book] Title: #{book.title}, Author: #{book.author}" }
+  end
+
+  def create_rental
+    puts 'Select which book you want to rent by entering its number'
+    @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
+
+    book_id = gets.chomp.to_i
+
+    puts 'Select a person from the list by its number'
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+
+    person_id = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp.to_s
+
+    rental = Rental.new(date, @people[person_id], @books[book_id])
+    @rentals << rental
+
+    puts
+    puts 'Rental created successfully'
+  end
+
+  def list_all_rentals
+    print 'To see person rentals enter the person ID: '
+    id = gets.chomp.to_i
+
+    puts
+    puts 'Rented Books:'
+    @rentals.each do |rental|
+      if rental.person.id == id
+        puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author} to #{rental.person.name}"
+      end
+    end
+  end
 end
