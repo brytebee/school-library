fca233a667f994964e5f4268e1bb4d615e8819e0
brytebee <brytebee@gmail.com>
2022-04-13 10:43:46 +0100

    :rotating_light: Spaces
    Added spaces

--- src/module/preserve.rb
+++ src/module/preserve.rb
@@ -2,7 +2,7 @@ module PreserveData
   def save_books
     data = []
     @books.each do |book|
-      data.push({author: book.author, title: book.title})
+      data.push( {author: book.author, title: book.title} )
     end
 
     File.open('./src/store/books.json', 'w') do |file|
