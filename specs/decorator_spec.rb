602f6e07b42a3f350fece4da37f5e8f80b902110
brytebee <brytebee@gmail.com>
2022-04-15 12:03:41 +0100

    :white_check_mark: Add test
    Added a couple of tests

--- specs/decorator_spec.rb
+++ specs/decorator_spec.rb
@@ -4,5 +4,13 @@ require 'rspec/autorun'
 describe Decorator do
   context 'when instantiated' do
     deco = Decorator.new('Joe')
+
+    it 'with a name' do
+      expect(deco.class).to eq(Decorator)
+    end
+    
+    it 'with a name' do
+      expect(deco.nameable).to eq('Joe')
+    end
   end
 end
