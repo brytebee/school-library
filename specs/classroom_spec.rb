aa016a06901f260922455a117e67c31f3487c236
Mwapsam <mwapsam@gmail.com>
2022-04-15 11:13:54 +0200

    Tested classroom and person classes

---
+++ specs/classroom_spec.rb
@@ -0,0 +1,30 @@
+require './src/classes/person'
+require './src/classes/student'
+require './src/classes/teacher'
+require './src/classes/classroom'
+
+describe Classroom do
+  before :each do
+    @classroom = Classroom.new(11)
+  end
+
+  it 'created classroom should instance of Classroom class' do
+    expect(@classroom).to be_instance_of(Classroom)
+  end
+
+  it 'should have label attribute' do
+    label = @classroom.label
+    expect(label).to eq 11
+  end
+
+  it 'should have students attribute' do
+    students = @classroom.students
+    expect(students).to eq []
+  end
+
+  it 'should add student to classroom' do
+    student = Student.new(18, 'Mwape', parent_permission: true)
+    @classroom.add_student(student)
+    expect(@classroom.students.length).to eq 1
+  end
+end
---
+++ specs/person_spec.rb
@@ -0,0 +1,23 @@
+require './src/classes/person'
+
+describe Person do
+  context 'before each test in the person class' do
+    before(:each) do
+      @person = Person.new(18, 'Mwape', parent_permission: true)
+    end
+
+    describe '#new' do
+      it 'should take two parameter and returns a person' do
+        expect(@person).to be_an_instance_of Person
+      end
+
+      it 'should have an age' do
+        expect(@person.age).to eql(18)
+      end
+
+      it 'should have a name' do
+        expect(@person.name).to eql('Mwape')
+      end
+    end
+  end
+end
