e8a08f6bbe6f0342921bec000038c00d07d7a4b9
brytebee <brytebee@gmail.com>
2022-04-15 11:48:28 +0100

    :fire: Clean up
    Cleaned up

--- src/classes/rental.rb
+++ src/classes/rental.rb
@@ -1,15 +1,13 @@
 class Rental
   attr_accessor :date, :person, :books, :rentals
 
-  # attr_reader :person, :book
-
   def initialize(date, person, book)
     @date = date
 
     @person = person
-    # @person.rentals << self
+    @person.rentals << self
 
     @books = book
-    # @books.rentals << self
+    @books.rentals << self
   end
 end
